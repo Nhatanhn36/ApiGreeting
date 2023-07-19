40243f85882779ed576ae3552e2afe9b90d2da40
Nhatanhn36 <96851306+Nhatanhn36@users.noreply.github.com>
2023-07-19 14:46:48 +0700

    Initial commit

---
+++ src/main/java/anhnnth/codelean/apigreeting/ApiGreetingApplication.java
@@ -0,0 +1,13 @@
+package anhnnth.codelean.apigreeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGreetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGreetingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/anhnnth/codelean/apigreeting/ApiGreetingApplicationTests.java
@@ -0,0 +1,13 @@
+package anhnnth.codelean.apigreeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGreetingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
